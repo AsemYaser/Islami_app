349bb3dcc536262b17e3e0132dd1a312ca74d887
Asem <asem.y88@gmail.com>
2025-09-16 11:19:16 +0300

    + Initialize project. + Added empty home screen.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/ui/home/home_screen.dart';
+
+void main() {
+  runApp(IslamiApp());
+}
+
+class IslamiApp extends StatelessWidget {
+  const IslamiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+    );
+  }
+}
